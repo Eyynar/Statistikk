bfbb4e6717ffc53dcc77c6e0601c0968d29f8338
Eyynar <89594643+Eyynar@users.noreply.github.com>
2023-02-24 10:40:15 +0100

    Round oblig3

--- Oblig3/oppg10_b.py
+++ Oblig3/oppg10_b.py
@@ -3,4 +3,4 @@ from scipy import stats
 
 i = int(input("Skriv inn ønsket vekt: \n"))
 
-print(f"Sannsynligheten for at brødet veier mer enn {i} gram: {1 - (stats.norm.cdf(i, 760, 10))}")
+print(f"Sannsynligheten for at brødet veier mer enn {i} gram: {1 - round(stats.norm.cdf(i, 760, 10), 3)}")
--- Oblig3/oppg3.py
+++ Oblig3/oppg3.py
@@ -3,24 +3,20 @@ import numpy as np
 
 
 # Oppgave a
-print(f"Oppgave a:\nP(X=3): {stats.hypergeom.pmf(3, 64, 14, 8)}\n"
-      f"P(X<=3): {stats.hypergeom.cdf(3, 64, 14, 8)}\n\n")
+print(f"Oppgave a:\nP(X=3): {round(stats.hypergeom.pmf(3, 64, 14, 8), 4)}\n"
+      f"P(X<=3): {round(stats.hypergeom.cdf(3, 64, 14, 8), 4)}\n\n")
 
 
 # Oppgave b
-bin_pmf = stats.binom.pmf((np.arange(65)), 8, (14/64))
-
-print(f"Oppgave b:\nP(X=3): {bin_pmf[3]}\n"
-      f"P(X<=3): {stats.binom.cdf(3, 8, (14/64))}\n\n")
+print(f"Oppgave b:\nP(X=3): {round(stats.binom.pmf(3, 8, (14/64)),4)}\n"
+      f"P(X<=3): {round(stats.binom.cdf(3, 8, (14/64)), 4)}\n\n")
 
 
 # Oppgave c
-print(f"Oppgave c:\nP(Y=3): {stats.hypergeom.pmf(3, 640, 140, 8)}\n"
-      f"P(Y<=3): {stats.hypergeom.cdf(3, 640, 140, 8)}\n\n")
+print(f"Oppgave c:\nP(Y=3): {round(stats.hypergeom.pmf(3, 640, 140, 8), 4)}\n"
+      f"P(Y<=3): {round(stats.hypergeom.cdf(3, 640, 140, 8), 4)}\n\n")
 
 
 # Oppgave d
-bin_pmf = stats.binom.pmf((np.arange(641)), 8, (140/640))
-
-print(f"Oppgave d:\nP(Y=3): {bin_pmf[3]}\n"
-      f"P(Y<=3): {stats.binom.cdf(3, 8, (140/640))}\n\n")
+print(f"Oppgave d:\nP(Y=3): {round(stats.binom.pmf(3, 8, (140/640)), 4)}\n"
+      f"P(Y<=3): {round(stats.binom.cdf(3, 8, (140/640)), 4)}\n\n")
