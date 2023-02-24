7f69426877abeafb4d708ad35e2f51573205c812
Eyynar <89594643+Eyynar@users.noreply.github.com>
2023-02-24 10:26:14 +0100

    oppg_b

--- Oblig3/oppg10_b.py
+++ Oblig3/oppg10_b.py
@@ -2,6 +2,5 @@ import numpy as np
 from scipy import stats
 
 i = int(input("Skriv inn ønsket vekt: \n"))
-x = np.arange(720, 801, 1)
 
-print(f"Sannsynligheten for at brødet veier mer enn {i} gram: {1 - (stats.norm.cdf(x, 760, 10))[i-720]}")
+print(f"Sannsynligheten for at brødet veier mer enn {i} gram: {1 - (stats.norm.cdf(i, 760, 10))}")
