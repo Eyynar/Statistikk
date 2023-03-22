906c9b4f198ad7353ec707c39c7f9606f66c572b
Eyynar <89594643+Eyynar@users.noreply.github.com>
2023-03-22 17:21:25 +0100

    Oppgave 8

--- Oblig5/Oppg_8.py
+++ Oblig5/Oppg_8.py
@@ -1,4 +1,5 @@
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+from scipy import stats
 import pandas as pd
 
 measurements = pd.read_csv('katters_vekt.csv', quotechar=',', skipinitialspace=True).values
@@ -9,5 +10,14 @@ h_weights = []
 for row in measurements:
     b_weights.append(row[1])
     h_weights.append(row[2])
+
+#plt.scatter(b_weights, h_weights)
+#plt.show()
+
+reg_lin = stats.linregress(b_weights, h_weights)
+print(f"Ligningen for linjen: y={round(reg_lin.intercept, 4)}+{round(reg_lin.slope, 4)}x")
+print(f"Standardfeilen for estimatoren β er {round(reg_lin.stderr, 4)}")
+
+y = float(input())
 
     
